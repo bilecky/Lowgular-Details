ebb2284e068d748b72d8dbc247f4e7877ee217a3
bilecky <bilecky95@gmail.com>
2022-12-09 17:23:27 +0100

    exc5 - create user details

---
+++ src/app/models/user.model.ts
@@ -0,0 +1,11 @@
+export interface UserModel {
+  readonly id: number;
+  readonly email: string;
+  readonly username: string;
+  readonly password: string;
+  readonly name: any;
+  readonly address: object;
+  readonly phone: string;
+  readonly firstname: string;
+  readonly lastname: string;
+}
